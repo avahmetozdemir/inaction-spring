e1b0fbdbc2486d5117a2d056f17766568d64148b
avahmetozdemir <avamertozdemir@gmail.com>
2023-02-07 11:45:49 +0300

    add advertisement model and elastic search client config

--- src/main/java/com/lawman/inaction/advertisement/model/Advertisement.java
+++ src/main/java/com/lawman/inaction/advertisement/model/Advertisement.java
@@ -1,6 +1,34 @@
 package com.lawman.inaction.advertisement.model;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Document(indexName = "advertisement")
 public class Advertisement {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Keyword)
+    private String title;
+
+    @Field(type = FieldType.Keyword)
+    private String description;
+
+    private Double price;
+
+    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
+    private Date creationDate;
+
+    @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
+    private Date lastModifiedDate;
+
 
 
 }
